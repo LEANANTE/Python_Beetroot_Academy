38057752be40b45826315fc3f0af8da728e8f0f8
LEANANTE <georgy.levitsky@gmail.com>
2025-09-27 10:06:38 +0300

    вихідний результат завдань

---
+++ Tasks/2/task2.py
@@ -0,0 +1,28 @@
+# 'Hello World'
+print("Hello World")
+
+# 'Buryachok Forever'
+print("Buryachok", "Forever")
+
+# 'A_B_C' (sep)
+print("A", "B", "C", sep="_")
+
+# 'Hello***World***!' (sep)
+print("Hello", "World", "!", sep="***")
+
+# 'Python 3.13'
+print("Python 3.13", end="")
+
+# 'coding is fun!' (продовження попереднього рядка)
+print("coding is fun!")
+
+# 'One->Two->Three'
+print("One", "Two", "Three", sep="->", end="\n\n")
+
+# 'A B C D E'
+print("A", "B", "C", "D", "E")
+
+# 'Name: Jorik, Age: 33'
+name = "Jorik"
+age = 33
+print("Name:", name, "Age:", age, sep=" ", end=".\n")
